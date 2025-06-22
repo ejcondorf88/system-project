528b00e2732a02589d845925c8a24a8a929b8f6c
Elian Condor <econdor@ec.krugercorp.com>
2025-06-22 14:20:20 -0500

    Agrega el componente de perfil de usuario y actualiza las rutas para incluirlo. Se añade la dependencia 'react-icons' en el archivo package.json.

--- fronted/src/routes/AppRouter.tsx
+++ fronted/src/routes/AppRouter.tsx
@@ -1,5 +1,6 @@
 import { Chat } from "@/components/Chat";
 import Login from "@/components/Login";
+import { Profile } from "@/components/Profile";
 import { Register } from "@/components/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from "./pages/Home";
@@ -12,6 +13,7 @@ function AppRouter() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/chat" element= {<Chat/>}/>
+        <Route path="/profile" element= {<Profile/>}/>
       </Routes>
     </BrowserRouter>
   );
